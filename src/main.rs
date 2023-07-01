c266487d3b3c186cbd5bf2becbf36a42fcbc7f6f
Prajwal-I <46865198+Prajwal-I@users.noreply.github.com>
2023-07-02 00:25:46 +0530

    hello word++

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Thou shall be known as what?");
+    let mut name = String::new();
+    let greeting = "Namaste 🙏🙏🙏";
+    io::stdin().read_line(&mut name)
+        .expect("Your name hasn't pleased though toungue, Repeat!");
+    println!("Hello, {}! {}",name.trim_end() ,greeting);
 }
